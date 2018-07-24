782551c1bdd695d42993b151e064e4e30cd0f252
zhouzhichao <342412187@qq.com>
2018-07-24 20:47:12 +0800

    'list列表添加删除功能'

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+入口js
+ */
+import Vue from 'vue'
+import App from './App'
+
+import './main.css'
+new Vue({
+  el: '#app',
+  components: {  //注册组件
+    App
+  },
+  template: '<App/>'  //将App标签渲染到el对应的div中
+})
